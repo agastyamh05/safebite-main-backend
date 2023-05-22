c4ca96da762748e1a32963cb69dfec072a4699c1
Surya Adi <kmsurya.adi44@gmail.com>
2023-05-22 12:20:58 +0800

    remove dist

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const users_route_1 = require("./routes/users.route");
-const validateEnv_1 = require("./utils/config/validateEnv");
-(0, validateEnv_1.ValidateEnv)();
-const app = new app_1.App([new users_route_1.UsersRoute()]);
-app.listen();
